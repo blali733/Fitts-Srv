bdb226d031b45453f57f576bb68b8c9ef0ad733b
Michał Urbaniak <blali733@gmail.com>
2018-04-28 11:20:09 +0200

    Project structure.

---
+++ Assets/Scripts/ConfigSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct NetworkConfig
+{
+    public string address;
+    public string port;
+    public NetworkConfig(string address, string port)
+    {
+        this.address = address;
+        this.port = port;
+    }
+}
+
+public class ConfigSingleton {
+    //Singleton instance
+    private static ConfigSingleton instance;
+
+    //Variables:
+    private NetworkConfig networkConfig = new NetworkConfig();
+
+    //Instance getter
+    public static ConfigSingleton getInstance()
+    {
+        if (instance == null)
+            instance = new ConfigSingleton();
+        return instance;
+    }
+
+    public NetworkConfig getNetworkConfig()
+    {
+        return this.networkConfig;
+    }
+
+    public void setNetworkConfig(NetworkConfig networkConfig)
+    {
+        this.networkConfig = networkConfig;
+    }
+}
---
+++ Assets/Scripts/MainLoop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLoop : MonoBehaviour {
+
+    private string secretKeyR = "_4T%rAcXd794@Vg!_ghtGH=yWv-z5_Cy";
+    private string secretKeyW = "bR@RCNYHdj3!_a3#BR=%TPs=*SNY5cX$";
+    public string addUserURL = "http://blali733.dynu.com/unitydb/add_user.php?"; //be sure to add a ? to your url
+    public string listUserURL = "http://blali733.dynu.com/unitydb/list_user.php";
+
+    // Use this for initialization
+    void Start () {
+        Application.targetFrameRate = 10;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
