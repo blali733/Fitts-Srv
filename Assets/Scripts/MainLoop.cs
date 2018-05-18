7a4610b817b2caa6c0b5b85ee3cd2a87ae83f088
Michał Urbaniak <blali733@gmail.com>
2018-05-18 21:34:09 +0200

    Changed method of address string construction.

--- Assets/Scripts/MainLoop.cs
+++ Assets/Scripts/MainLoop.cs
@@ -6,8 +6,9 @@ public class MainLoop : MonoBehaviour {
 
     private string secretKeyR = "_4T%rAcXd794@Vg!_ghtGH=yWv-z5_Cy";
     private string secretKeyW = "bR@RCNYHdj3!_a3#BR=%TPs=*SNY5cX$";
-    public string addUserURL = "http://blali733.dynu.com/unitydb/add_user.php?"; //be sure to add a ? to your url
-    public string listUserURL = "http://blali733.dynu.com/unitydb/list_user.php";
+    private string addUserURL = "add_user.php?"; //be sure to add a ? to your url
+    private string listUserURL = "list_user.php";
+    public string domain;
 
     // Use this for initialization
     void Start () {
