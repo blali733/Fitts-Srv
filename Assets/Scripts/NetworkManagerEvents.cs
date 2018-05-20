5edce2470a67b506525025987e88bcfa3ab8622f
Michał Urbaniak <blali733@gmail.com>
2018-05-20 15:11:52 +0200

    Added debugging info.

--- Assets/Scripts/NetworkManagerEvents.cs
+++ Assets/Scripts/NetworkManagerEvents.cs
@@ -8,11 +8,11 @@ using SharedMessages;
 public class NetworkManagerEvents : NetworkManager
 {
     private ConfigSingleton _config;
-    private List<NetworkConnection> _connections;
+    //private List<NetworkConnection> _connections;
     private void Start()
     {
         _config = ConfigSingleton.GetInstance();
-        _connections = new List<NetworkConnection>();
+        //_connections = new List<NetworkConnection>();
     }
 
     public override void OnStartServer()
@@ -23,13 +23,6 @@ public class NetworkManagerEvents : NetworkManager
         NetworkServer.RegisterHandler(MyMsgType.TargetInfos, GetTargetInfos);
     }
 
-    public override void OnServerConnect(NetworkConnection conn)
-    {
-        _connections.Add(conn);
-        TestCasesMessage msg = new TestCasesMessage(_config.TestCases);
-        NetworkServer.SendToClient(conn.connectionId, MyMsgType.TestCases, msg);
-    }
-
     public void GetTargetDatas(NetworkMessage message)
     {
 
@@ -38,5 +31,18 @@ public class NetworkManagerEvents : NetworkManager
     public void GetTargetInfos(NetworkMessage message)
     {
 
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        Debug.Log(string.Format("Client with id {0}, from {1} has connected.", conn.connectionId, conn.address));
+        //_connections.Add(conn);
+        TestCasesMessage msg = new TestCasesMessage(_config.TestCases);
+        NetworkServer.SendToClient(conn.connectionId, MyMsgType.TestCases, msg);
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        Debug.Log(string.Format("Client with id {0} has disconnected.", conn.connectionId));
     }
 }
